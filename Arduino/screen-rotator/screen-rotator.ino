8802e5e494a9f0e0ed5e69d1eb09d01258c4d4d6
dwminer <daltonminer@gmail.com>
2016-03-15 19:03:03 -0400

    Arduino: Add Randr rotation defs, comapre accel output to hardcoded values and output rotation value

--- Arduino/screen-rotator/screen-rotator.ino
+++ Arduino/screen-rotator/screen-rotator.ino
@@ -1,11 +1,27 @@
+#include <math.h>
+//write our own round if math.h is too big?
+
 #define XPIN  A0
 #define YPIN  A1
 #define ZPIN  A2
-
+/*
+ * Turning a normally oriented screen clockwise correponds to 
+ * RR_Rotate_90
+ * Assuming accelerometer is mounted upright, reversed on the back of the monitor:
+ * 0: x = 0, y = -1
+ * 90: x = -1, y = 0
+ * 180: x = 0, y = 1
+ * 270: x = 1, y = 0
+ */
+#define RR_Rotate_0             1
+#define RR_Rotate_90            2
+#define RR_Rotate_180           4
+#define RR_Rotate_270           8
 
 int vals[3]; //x,y,z
-int offsets[3];
 float scaledVals[3];
+int x, y;
+int current_rotation, new_rotation;
 int i;
 float scaledX;
 
@@ -18,12 +34,6 @@ void setup() {
   vals[0] = analogRead(XPIN);
   vals[1] = analogRead(YPIN);
   vals[2] = analogRead(ZPIN);
-
-  i = 0;
-  for (i = 0; i < 3; i++)
-  {
-    offsets[i] = 0;//675/2 - vals[i];
-  }
   
 }
 
@@ -35,11 +45,26 @@ void loop() {
   i = 0;
   for (i = 0; i < 3; i++)
   {
-    scaledVals[i] = mapf(vals[i] + offsets[i], 0, 675, -3.0, 3.0);
-    Serial.print(scaledVals[i]);
-    Serial.print(" ");
+    scaledVals[i] = mapf(vals[i], 0, 675, -3.0, 3.0);
   }
-  Serial.print("\n\n");
+  
+  x = round(scaledVals[0]);
+  y = round(scaledVals[1]);
+  if (x == 0 && y == -1)
+    new_rotation = RR_Rotate_0;
+  else if (x == -1 && y == 0)
+    new_rotation = RR_Rotate_90;
+  else if (x == 0 && y == 1)
+    new_rotation = RR_Rotate_180;
+  else if (x == 1 && y == 0)
+    new_rotation = RR_Rotate_270;
+
+  if (current_rotation != new_rotation)
+  {
+    current_rotation = new_rotation;
+    Serial.print(new_rotation);
+    Serial.print("\n");
+  }
 
   delay(500);
 }
