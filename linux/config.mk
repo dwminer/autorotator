92c9e69d0d78ac4c4c5a5f687c06ad5d520e3976
dwminer <daltonminer@gmail.com>
2016-03-17 15:36:56 -0400

    Initial config/makefile

---
+++ linux/config.mk
@@ -0,0 +1,12 @@
+PREFIX = /usr
+
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/X11
+
+INCS = -I${X11INC}
+LIBS = -L${X11LIB} -lX11 -lXrandr
+
+CFLAGS = -Wall -Os ${INCS}
+LDFLAGS = -s ${LIBS}
+
+CC = gcc
